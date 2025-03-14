29801b3a2c31ae9ff3e0758132e78d03bfe34108
MarkSian <merksianipar@gmail.com>
2025-03-14 15:07:55 -0600

    Create index.js
    set const variables

---
+++ index.js
@@ -0,0 +1,15 @@
+/* Part 1: Growing Pains*/ 
+//1. Declare you global constansts here
+
+const PI = 3.1415;
+const radius = 5;
+const area = PI * radius * radius;
+const plantSpace = 0.8;
+const startingPlants = 20;
+const maximumSpace = area / plantSpace;
+
+
+/* math operators*/ 
+//math.pow(2, weeks);
+/* math operators*/
+
